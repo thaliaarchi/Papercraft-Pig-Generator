750084c0f6d36ddb389aee04bc1f5eb44c43b0c2
Andrew Archibald <andrew@robarchibald.com>
2015-07-09 18:30:49 -0700

    Started converting to use Typescript

--- dev/temp/script.js
+++ dev/temp/script.js
@@ -23,25 +23,6 @@
 // 28 Feb 2015 TepigMC: Compacted labels and titles into sprite files; Added "Advanced (Standard)" head
 //                      Added "Show Helmet Overlay" option; Added texture options
 //
-// NOTES:
-//
-// This generator creates a pig papercraft with an optional saddle, helmet, and boots.
-// There are also options for a flat nose and simple head that don't follow real minecraft, but are easier to make.
-// There is also a bonus matching ultra mini pig that was inspired by Maki.
-//
-// Sizing:
-//   The saddle, helmet, and boots can vary in texture size, so there may need to be some trimming to fit it over the pig.
-//
-//   The accessories page (second page) works the best if printed slightly larger than the pig page so it fits onto the pig better.
-//   In Windows Photo Viewer, this can be solved by:
-//     - Unchecking 'Fit picture to frame' when you print the pig page
-//     - and Checking 'Fit picture to frame' for the accessories page.
-//
-// Textures:
-//   - Pig texture found at "assets\minecraft\textures\entity\pig\pig.png"
-//   - Saddle texture found at "assets\minecraft\textures\entity\pig\pig_saddle.png"
-//   - Armor texture (Layer 1) found at "assets\minecraft\textures\models\armor\diamond_layer_1.png"
-//
 ////////////////////////////////////////////////////////////////////////////////
 
 // Input names
